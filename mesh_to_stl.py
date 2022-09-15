55f6ef17e01ae9640f58f706812be59d1e4a7daf
Exr0n <mail@exr0n.com>
2022-09-14 22:50:24 -0700

    dece windy cloth sim

--- mesh_to_stl.py
+++ mesh_to_stl.py
@@ -10,10 +10,16 @@ from dataclasses import dataclass
 GRAVITY = np.array([0, -0.1])
 FRICTION = 0.9999
 
+NUM_RESOLVE_STEPS = 8
+
+N_COLS = 12
+N_ROWS = 12
+
 @dataclass
 class Point:
     pos: np.ndarray
     old_pos: np.ndarray
+    pinned: bool
 
 @dataclass
 class Stick:
@@ -25,46 +31,62 @@ def distance(p1: Point, p2: Point):
     return np.linalg.norm(p1.pos - p2.pos)
 
 points = [
-    Point(pos=np.array([x, y]), old_pos=np.array([x, y]))
-    for x in np.linspace(-10, 1, 12) for y in np.linspace(-10, 1, 12) ]
+    Point(pos=np.array([x, y]), old_pos=np.array([x, y]), pinned=False)
+    for x in np.linspace(-10, 1, N_COLS) for y in np.linspace(-10, 1, N_ROWS) ]
 
-pinned_points = [ x for x in points if x.pos[1] > 0.5 ]
+for i, p in enumerate(points):
+    if i % N_ROWS == N_ROWS-1 and np.random.random() < 0.3:
+        p.pinned = True
 
-sticks = [
-    Stick(p_a=points[0], p_b=points[-1], len=distance(points[0], points[-1]))
-        ]
+def make_stick_from_indicies(c1, r1, c2, r2):
+    p1 = points[c1*N_ROWS + r1]
+    p2 = points[c2*N_ROWS + r2]
+    return Stick(p_a=p1, p_b=p2, len=distance(p1, p2))
+
+sticks = [ make_stick_from_indicies(c, r, c+1, r) for c in range(N_COLS-1) for r in range(N_ROWS) ] \
+        + [ make_stick_from_indicies(c, r, c, r+1) for c in range(N_COLS) for r in range(N_ROWS-1)  ]
 
 def motion_kinematics(points):
-    for p in points:
+    for i, p in enumerate(points):
+        if p.pinned: continue
         # print(p.pos, p.old_pos)
         vel = p.pos - p.old_pos
         p.old_pos = p.pos.copy()
         p.pos += vel * FRICTION
-        if p.pos[1] <= 0:
-            p.pos += GRAVITY
+        p.pos += GRAVITY
+        if i == 0:  # pull the bottom left point away to check that side connections exist
+            p.pos += np.array([-0.7, 0.0])
 
 def motion_rigidsticks(points):
     for stick in sticks:
+        if stick.p_a.pinned and stick.p_b.pinned: continue
         # just straight up moves points towards/away from each other
         # as per https://www.youtube.com/watch?v=pBMivz4rIJY
         point_offset = stick.p_b.pos - stick.p_a.pos
         point_distance = np.linalg.norm(point_offset)
-        offset = point_offset * (stick.len - point_distance)/point_distance/2
+        offset = point_offset * (stick.len - point_distance)/point_distance
 
-        print(point_offset, point_distance, offset)
+        # print(point_offset, point_distance, offset)
 
-        stick.p_a.pos -= offset
-        stick.p_b.pos += offset
+        if stick.p_a.pinned:
+            stick.p_b.pos += offset
+        elif stick.p_b.pinned:
+            stick.p_a.pos -= offset
+        else:
+            stick.p_a.pos -= offset/2
+            stick.p_b.pos += offset/2
 
-def motion_pinned(points):
-    for p in pinned_points:
-        p.pos = p.old_pos
+# def motion_pinned(points):
+#     for p in pinned_points:
+#         p.pos = p.old_pos.copy()
 
 plot_x, plot_y = [], []
 def animate(i):
     motion_kinematics(points)
-    motion_rigidsticks(points)
-    motion_pinned(points)
+    # resolution handlers
+    for _ in range(NUM_RESOLVE_STEPS):
+        motion_rigidsticks(points)
+    # motion_pinned(points)
     global plot_x, plot_y
     plot_x = [p.pos[0] for p in points]
     plot_y = [p.pos[1] for p in points]
